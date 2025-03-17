e58e022c045b3e87b9b8972b75711cd9d5dc57d9
SamRawdon <sr6360@nyu.edu>
2025-03-16 22:41:59 -0400

    Edited header + album page

--- front-end/src/AlbumPage.js
+++ front-end/src/AlbumPage.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./AlbumPage.css";
 
 const AlbumPage = ({ album }) =>{
   if(!album) return <div>No album selected</div>;
--- front-end/src/App.js
+++ front-end/src/App.js
@@ -14,33 +14,29 @@ import Footer from "./components/Footer";
 import "./App.css";
 
 const newlyAddedAlbums = [
-  { id: 1, title: "Album 1", imageUrl: "/bitchesbrew.png" },
-  { id: 2, title: "Album 2", imageUrl: "/bitchesbrew.png" },
-  { id: 3, title: "Album 3", imageUrl: "/bitchesbrew.png" },
-  { id: 4, title: "Album 4", imageUrl: "/bitchesbrew.png" },
-  { id: 5, title: "Album 5", imageUrl: "/bitchesbrew.png" },
-  { id: 6, title: "Album 6", imageUrl: "/bitchesbrew.png" },
-  { id: 7, title: "Album 7", imageUrl: "/bitchesbrew.png" },
-  { id: 8, title: "Album 8", imageUrl: "/bitchesbrew.png" },
-  { id: 9, title: "Album 9", imageUrl: "/bitchesbrew.png" },
-  { id: 10, title: "Album 10", imageUrl: "/bitchesbrew.png" },
-  { id: 11, title: "Album 11", imageUrl: "/bitchesbrew.png" },
-  { id: 12, title: "Album 12", imageUrl: "/bitchesbrew.png" },
+  { id: 1, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 2, title: "Bitches Brew 2", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 3, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 4, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 5, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 6, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 7, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 8, title: "Bitches Brew 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
 ];
 
 const staffFavorites = [
-  { id: 13, title: "Staff Favorite 1", imageUrl: "/bitchesbrew.png" },
-  { id: 14, title: "Staff Favorite 2", imageUrl: "/bitchesbrew.png" },
-  { id: 15, title: "Staff Favorite 3", imageUrl: "/bitchesbrew.png" },
-  { id: 16, title: "Staff Favorite 4", imageUrl: "/bitchesbrew.png" },
-  { id: 17, title: "Staff Favorite 5", imageUrl: "/bitchesbrew.png" },
-  { id: 18, title: "Staff Favorite 6", imageUrl: "/bitchesbrew.png" },
-  { id: 19, title: "Staff Favorite 7", imageUrl: "/bitchesbrew.png" },
-  { id: 20, title: "Staff Favorite 8", imageUrl: "/bitchesbrew.png" },
-  { id: 21, title: "Staff Favorite 9", imageUrl: "/bitchesbrew.png" },
-  { id: 22, title: "Staff Favorite 10", imageUrl: "/bitchesbrew.png" },
-  { id: 23, title: "Staff Favorite 11", imageUrl: "/bitchesbrew.png" },
-  { id: 24, title: "Staff Favorite 12", imageUrl: "/bitchesbrew.png" },
+  { id: 13, title: "Staff Favorite 1", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 14, title: "Staff Favorite 2", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 15, title: "Staff Favorite 3", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 16, title: "Staff Favorite 4", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 17, title: "Staff Favorite 5", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 18, title: "Staff Favorite 6", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 19, title: "Staff Favorite 7", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 20, title: "Staff Favorite 8", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 21, title: "Staff Favorite 9", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 22, title: "Staff Favorite 10", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 23, title: "Staff Favorite 11", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
+  { id: 24, title: "Staff Favorite 12", imageUrl: "/bitchesbrew.png", artist: "Miles Davis", genre: "Jazz", format: "Vinyl" },
 ];
 
 const staffContacts = [
