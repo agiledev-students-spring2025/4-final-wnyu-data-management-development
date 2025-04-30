2adce89bc2f4fb56422897df728fa427f12037c7
SamRawdon <sr6360@nyu.edu>
2025-04-30 03:31:08 -0400

    Restore API_URLs

--- front-end/src/SearchResults.js
+++ front-end/src/SearchResults.js
@@ -18,8 +18,8 @@ const SearchResults = () => {
   useEffect(() => {
     const fetchResults = async () => {
       try {
-        //const res = await fetch(`${process.env.REACT_APP_API_URL}api/search?type=${searchType}&query=${searchQuery}`);
-        const res = await fetch(`http://localhost:8080/api/search?type=${searchType}&query=${searchQuery}`);
+        const res = await fetch(`${process.env.REACT_APP_API_URL}api/search?type=${searchType}&query=${searchQuery}`);
+        //const res = await fetch(`http://localhost:8080/api/search?type=${searchType}&query=${searchQuery}`);
         const data = await res.json();
         setResults(data);
       } catch (err) {
